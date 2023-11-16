eaca107f453a483afd0d5bb16b3f3dcfbb63631f
sedith <martin.jacquet@posteo.net>
2023-11-16 18:34:50 +0100

    automatic generation of pdf
    the html is rendered to pdf using chromium through pyppeteer
    the css uses grid which is not supported by most html->pdf tools
    (wkhtmltopdf, weasyprint, prince), this is the "easiest" solution,
    although scripting through puppeteer is most likely overkill

--- agenda_gen/month_gen.py
+++ agenda_gen/month_gen.py
@@ -1,10 +1,15 @@
 import numpy as np
 import os
+import asyncio
+from pyppeteer import launch
+from pypdf import PdfWriter, PdfReader
+from io import BytesIO
+
 
 def month_grid(nbdays, d0):
     """Defines an integer grid for the whole month.
     Each row defines a week, each collumn is a weekday. Cell values are the date for that day, or 0 otherwise.
-    nbdays   - nb of days in the month (28-31)
+    nbdays  - nb of days in the month (28-31)
     d0      - index of first day of month in week (1: monday, 7: sunday)
     Example: month of 30 days, starting on a tuesday:
         [[ 0.,  1.,  2.,  3.,  4.,  5.,  6.],
@@ -25,6 +30,7 @@ def month_grid(nbdays, d0):
 
 if __name__ == '__main__':
     ## parameters
+    pdf = 1  # 1 for pdf, 0 html
     month_name = 'Décembre'
     nbdays = 31
     d0 = 5
@@ -37,13 +43,12 @@ if __name__ == '__main__':
 
     ## process template
     month = month_grid(nbdays, d0)
-    print(month)
 
+    html_list = []
     for week_idx, week in enumerate(month):
         output_path = os.path.join(folder, '..', 'output', 'week' + str(week_idx))
         if not os.path.exists(output_path):
             os.makedirs(output_path)
-
         ## week dates
         non_zero_days_idx = np.nonzero(week)[0]
         date_start = week[non_zero_days_idx[0]]
@@ -56,10 +61,14 @@ if __name__ == '__main__':
         with open(os.path.join(input_path,'style-lun-mer.css'), 'r') as f:
             css = f.read()
 
+        if week_idx == 0 and non_zero_days_idx[0] != 0:  # remove perso/boulot if the month is not starting on a Monday
+            html = html.replace('perso', '')
+            html = html.replace('boulot', '')
+
         html = html.replace('WEEK_DATES', week_title)
-        html = html.replace('MON_DATE', 'Lun ' + str(week[0])) if week[0] else html.replace('MON_DATE', '')
-        html = html.replace('TUE_DATE', 'Mar ' + str(week[1])) if week[1] else html.replace('TUE_DATE', '')
-        html = html.replace('WED_DATE', 'Mer ' + str(week[2])) if week[2] else html.replace('WED_DATE', '')
+        html = html.replace('MON_DATE', 'lun ' + str(week[0])) if week[0] else html.replace('MON_DATE', '')
+        html = html.replace('TUE_DATE', 'mar ' + str(week[1])) if week[1] else html.replace('TUE_DATE', '')
+        html = html.replace('WED_DATE', 'mer ' + str(week[2])) if week[2] else html.replace('WED_DATE', '')
 
         if week[0]:
             css = css.replace('ROW_1', 'lundi boulot')
@@ -79,6 +88,8 @@ if __name__ == '__main__':
         with open(os.path.join(output_path, 'style-lun-mer.css'), 'w') as f:
             f.write(css)
 
+        html_list.append(os.path.join(output_path, 'lun-mer.html'))
+
 
         ## thu-sun
         with open(os.path.join(input_path, 'jeu-dim.html'), 'r') as f:
@@ -86,10 +97,14 @@ if __name__ == '__main__':
         with open(os.path.join(input_path, 'style-jeu-dim.css'), 'r') as f:
             css = f.read()
 
+        if week_idx == 0 and non_zero_days_idx[0] > 3:  # remove perso/boulot if the month is starting starting after Thursday
+            html = html.replace('perso', '')
+            html = html.replace('boulot', '')
+
         html = html.replace('THU_DATE', 'Jeu ' + str(week[3])) if week[3] else html.replace('THU_DATE', '')
-        html = html.replace('FRI_DATE', 'Ven ' + str(week[4])) if week[4] else html.replace('FRI_DATE', '')
-        html = html.replace('SAT_DATE', 'Sam ' + str(week[5])) if week[5] else html.replace('SAT_DATE', '')
-        html = html.replace('SUN_DATE', 'Dim ' + str(week[6])) if week[6] else html.replace('SUN_DATE', '')
+        html = html.replace('FRI_DATE', 'ven ' + str(week[4])) if week[4] else html.replace('FRI_DATE', '')
+        html = html.replace('SAT_DATE', 'sam ' + str(week[5])) if week[5] else html.replace('SAT_DATE', '')
+        html = html.replace('SUN_DATE', 'dim ' + str(week[6])) if week[6] else html.replace('SUN_DATE', '')
 
         if week[3]:
             css = css.replace('ROW_1', 'jeudi boulot')
@@ -112,3 +127,27 @@ if __name__ == '__main__':
             f.write(html)
         with open(os.path.join(output_path, 'style-jeu-dim.css'), 'w') as f:
             f.write(css)
+
+        html_list.append(os.path.join(output_path, 'jeu-dim.html'))
+
+
+    async def get_pdf(html_list):
+        browser = await launch(headless=True, executablePath='/usr/bin/chromium-browser')
+        pdfs = []
+        for html_path in html_list:
+            page = await browser.newPage()
+            await page.goto('file://' + html_path)
+            pdfs.append(await page.pdf(format='A4'))
+        await browser.close()
+        return pdfs
+
+    pdfs = asyncio.get_event_loop().run_until_complete(get_pdf(html_list))
+
+    merger = PdfWriter()
+    for pdf in pdfs:
+        reader = PdfReader(BytesIO(pdf))
+        merger.append(reader)
+
+    output_file = os.path.join(folder, '..', 'output', 'agenda.pdf')
+    with open(output_file, 'wb') as f:
+        merger.write(f)
