2ee1e4dc49dc91b294e99d6e51c148513146a975
sedith <martin.jacquet@posteo.net>
2024-08-11 00:52:43 +0200

    allow generation of several months in a single pdf

--- scripts/make_agenda.py
+++ scripts/make_agenda.py
@@ -7,18 +7,22 @@ import datetime
 if __name__ == '__main__':
     ## get args
     parser = argparse.ArgumentParser(description='agenda generator')
-    parser.add_argument(dest='month', help='index of month (1: jan, 12: dec)', choices=range(1, 13), type=int)
-    parser.add_argument(dest='year', nargs='?', help='year', default=datetime.date.today().year , type=int)
+    parser.add_argument(dest='year', help='year', type=int)
+    parser.add_argument(dest='month', help='month', choices=range(1, 13), type=int)
+    parser.add_argument(dest='nb_months', nargs='?', help='number of months', default=1, choices=range(1, 13), type=int)
 
     args = parser.parse_args()
-
 
     ## load default config
     cfg = utils.load_cfg()
 
     ## get html
-    template = Template(cfg, args.year, args.month)
-    htmls = template.gen_html()
+    htmls = []
+    for i in range(args.nb_months):
+        m = (args.month + i - 1) % 12 + 1  # month index, 1-based
+        y = args.year if args.month + i < 13 else args.year + 1
+        template = Template(cfg, y, m)
+        htmls += template.gen_html()
 
     ## gen and save pdf
     pdfs = utils.html_to_pdf(htmls)
